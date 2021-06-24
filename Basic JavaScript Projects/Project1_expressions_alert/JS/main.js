e8af0e04f889b7027f7a4baccda90b717389b4ae
m131601 <77653698+m131601@users.noreply.github.com>
2021-06-23 19:25:39 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+document.write("Hello, World!");
+var A="This is a string";
+window.alert(A)
+document.write("Tommy said \"Go Home!\"");
+document.write("\Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+var B="Hello" + " Dolly"
+document.write(B)
+document.write(3 +3);
+function My_First_Function() {          //Defining a function and naming it
+    var str="This text is green";       //Defining a variable and giving it a 
+    var result= str.fontcolor("green"); //string value
+    document.getElementById("Green_Text").innerHTML=result; //Putting the value
+                                        //of result int"o HTML element with "Green_Text" id
+}
