d41b6f7f5a408096628c8a59cfbcb4536549bca1
m131601 <77653698+m131601@users.noreply.github.com>
2021-06-24 19:15:25 -0700

    Add files via upload

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -4,8 +4,11 @@ function My_First_Function() {          //Defining a function and naming it
     document.getElementById("Purple_Text").innerHTML=result; //Putting the value
                                         //of result into HTML element with "Green_Text" id                             //of result into HTML element with "Green_Text" id
 }
-function myFunction() {
+function My_First_Function() {
     var sentence ="I am learning";
     sentence =+ "a lot from this book!";
     document.getElementById("Concatenate").innerHTML=sentence;
 }
+function myFunction(7, 5) {
+    return 7 * 5;   // The function returns the product of 7 and 5
+  }
