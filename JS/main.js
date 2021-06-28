a8e041b4f82f0d6f08915193e7e651967544981d
m131601 <77653698+m131601@users.noreply.github.com>
2021-06-27 22:57:20 -0700

    Add files via upload
    Project5_comparisons

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,23 @@
-function my_Dictionary() {  //Dictionary for Animals
-    var Animal = {
-        Species:"Dog",
-        Color:"Black",
-        Breed:"Poodle",
-        Age:4,
-        Sound:"Woof!"
-    };
-    delete Animal.Sound; //Deleting the results
-    document.getElementById("Dictionay").innerHTML = Animal.Sound; //Display results
-}
+
+document.write("200" + 5); //Adding numbers together
+
+document.write(2E410); //infinite number output
+
+document.write(15>9); //Boolean logic true output
+document.write(15<9); //Boolean logic false output
+console.log(10+10); //displays vaue inside parenthesis
+document.write(10==10);//displays true output
+document.write(3==11);//displays false output
+x = 20;
+y = 20;
+document.write(x===y);
+x = 15;
+y = "15";
+document.write(x===y);
+a = "Love";
+b = "Love";
+document.write(x===y);
+document.write(6>2&&10>6)
+document.write(6>2&&10>6)
+document.write(6>2||10>6)
+document.write(6>2||10>30)
