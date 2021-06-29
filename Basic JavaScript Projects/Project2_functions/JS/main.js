4e0e8fe53b2a3961c2a38ca63490012da065c2fd
m131601 <77653698+m131601@users.noreply.github.com>
2021-06-29 14:43:46 -0400

    made corrections

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -5,8 +5,9 @@ function My_First_Function() {          //Defining a function and naming it
                                         //of result into HTML element with "Green_Text" id
 }
 
-function myFunction() {
-    return 7 * 5;   // The function returns the product of 7 and 5
+function mFunction(a, b) {
+    return a * b;   // The function returns the product of 7 and 5
   }
+  
     var y = 20; //Operator +=
     y += 10;
--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -4,3 +4,35 @@ function Ride_Function() {
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough!";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+function Vote_Function() {
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are too young":"You are old enough!";
+    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+}function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hunter", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructor").innerHTML =
+    "Erik drive a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + 
+    "manufactured in " + Erik.Vehicle_Year;
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+}
