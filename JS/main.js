b610ba1779c1b827e5970b95c33ac403a6e3bdab
m131601 <77653698+m131601@users.noreply.github.com>
2021-06-28 17:25:02 -0700

    Add files via upload
    PROJECT3_MATH_OPERATORS SUBMISSION ASSIGNMENT

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,36 @@
-function my_Dictionary() {  //Dictionary for Animals
-    var Animal = {
-        Species:"Dog",
-        Color:"Black",
-        Breed:"Poodle",
-        Age:4,
-        Sound:"Woof!"
-    };
-    delete Animal.Sound; //Deleting the results
-    document.getElementById("Dictionay").innerHTML = Animal.Sound; //Display results
+let  x = addition_Function; //Function is called, value will equal x
+
+function addition_Function(a, b) {  //Function returns a added to  b
+    return a + b;
 }
+let  x = subtraction_Function; //Function is called, value will equal x
+
+function subtraction_Function(a, b) {  //Function returns a subtracted by b
+    return a - b;
+}
+let  x = multiplication_Function; //Function is called, value will equal x
+
+function multiplication_Function(a, b) {  //Function returns a multiplied b
+    return a * b;
+}
+let  x = division_Function; //Function is called, value will equal x
+
+function division_Function(a, b) {  //Function returns a divided by  b
+    return a / b;
+}
+let  x = combination_Function; //Function is called, value will equal x
+
+function combination_Function(a, b) {  //Function returns a and b*c
+    return (a + b) *c;
+}
+let  x = modulus_Function; //Function is called, value will equal x
+
+function modulus_Function(a, b) {  //Function returns a and b
+    return a % b;
+}
+var x = 6;  //Increment 
+x++;
+var x = 6; //Decrement
+x++;
+
+ 
