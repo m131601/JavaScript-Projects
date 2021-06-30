c923f17938f38d6b5e8a8b6397fb817725266f31
m131601 <77653698+m131601@users.noreply.github.com>
2021-06-29 21:41:39 -0400

    Project6

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -9,7 +9,8 @@ function Vote_Function() {
     Age = document.getElementById("Age").value;
     Can_vote = (Age < 18) ? "You are too young":"You are old enough!";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
-}function Vehicle(Make, Model, Year, Color) {
+}
+function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Make = Make;
     this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
@@ -18,21 +19,8 @@ function Vote_Function() {
 var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hunter", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
-function myFunction() {
+function nFunction() {
     document.getElementById("Keywords_and_Constructor").innerHTML =
-    "Erik drive a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + 
-    "manufactured in " + Erik.Vehicle_Year;
-}
-
-
-
-
-}
-
-
-
-
-
-
-
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + 
+    " manufactured in " + Erik.Vehicle_Year;
 }
