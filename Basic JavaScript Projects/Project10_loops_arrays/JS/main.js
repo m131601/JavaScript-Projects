594bda7e2951389d2001149925c5452e886f4d2f
m131601 <77653698+m131601@users.noreply.github.com>
2021-06-30 23:50:34 -0400

    PROJECT10_LOOPS_ARRAYS SUBMISSION ASSIGNMENT

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -10,9 +10,24 @@ function count_To_Ten() {
 var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
 var Content = "";
 var Y;
-function for_Loop() {   //a for loop
+function for_Loop() {   // for loop
     for (Y = 0; Y< Instruments.length; Y++) {
     Content += Instruments[Y] + "<br>";
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+function cat_pics () {
+    var Cat_Picture = [];       //created an array of cat pictures
+    Cat_Picture [0] = "sleeping";
+    Cat_Picture [1] = "playing";
+    Cat_Picture [2] = "eating";
+    Cat_Picture [3] = "purring";
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " +
+    Cat_Picture[2] + ".";
+}
+var s = 92;
+document.write(s);
+{
+    let s = 44;
+    document.write("<br>" + s);
 }
